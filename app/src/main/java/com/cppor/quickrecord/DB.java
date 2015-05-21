d3ca79f1777958d94bafb7465167d9c9416a6868
willin <willin@cppor.com>
2015-05-22 07:29:53 +0800

    dev

---
+++ app/src/main/java/com/cppor/quickrecord/DB.java
@@ -0,0 +1,87 @@
+package com.cppor.quickrecord;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Build;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Created by willin on 5/15/15.
+ */
+public class DB extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME="quick_record_db";
+    private static final int DATABASE_VERSION = 1;
+
+    public static final String T_DEFINE ="T_DEFINE";
+    public static final int T_DEFINE_ID = 0;
+    public static final int T_DEFINE_NAME = 1;
+    public static final String[] T_DEFINE_ = new String[]{
+            "_ID","_NAME"
+    };
+
+    public static final String T_DATA = "T_DATA";
+    public static final int T_DATA_ID = 0;
+    public static final int T_DATA_VALUE = 1;
+    public static final int T_DATA_DEINE = 2;
+    public static final int T_DATA_TIME = 3;
+    public static final String[] T_DATA_ = new String[] {
+            "_ID", "_VALUE", "_DEFINE", "_TIME"
+    };
+
+
+    // android not define the
+
+    private String[] getTableCreateSqls() {
+        // table field define
+
+        String sql1 = "CREATE TABLE "+T_DEFINE+" ("+
+                T_DEFINE_[T_DEFINE_ID]+" INTEGER PRIMARY KEY autoincrement,"+
+                T_DEFINE_[T_DEFINE_NAME] +" TEXT"+
+                ")";
+
+        // table data
+        String sql2 = "CREATE TABLE "+T_DATA+" (" +
+                T_DATA_[T_DATA_ID] + " INTEGER PRIMARY KEY autoincrement," +
+                T_DATA_[T_DATA_VALUE] + " TEXT," +
+                T_DATA_[T_DATA_DEINE] + " INTEGER,"+
+                T_DATA_[T_DATA_TIME] + " DATETIME" +
+                ")";
+
+
+        return new String[] {sql1, sql2};
+    }
+
+
+    public DB(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public DB(Context context, SQLiteDatabase.CursorFactory factory) {
+        super(context, DATABASE_NAME, factory, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String[] sqls = getTableCreateSqls();
+        for (String s:sqls)
+            sqLiteDatabase.execSQL(s);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i2) {
+
+    }
+
+    public static String getDateTime(Date dt) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+        if (dt == null)
+            dt = new Date();
+
+        return format.format(dt);
+    }
+}
--- app/src/main/java/com/cppor/quickrecord/MainActivity.java
+++ app/src/main/java/com/cppor/quickrecord/MainActivity.java
@@ -1,17 +1,70 @@
 package com.cppor.quickrecord;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.sql.Date;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    TextView mTv;
+
+
+    class MyButtonClick implements View.OnClickListener
+    {
+        @Override
+        public void onClick(View view) {
+            Button button = (Button)view;
+            String a = mTv.getText().toString() + button.getText().toString();
+            mTv.setText(a);
+        }
+    }
+
+    MyButtonClick mClick = new MyButtonClick();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTv = (TextView)findViewById(R.id.textView);
+
+        int[] a = new int[]{R.id.button_0, R.id.button_1, R.id.button_2, R.id.button_3, R.id.button_4, R.id.button_5,
+                R.id.button_6,R.id.button_7,R.id.button_8,R.id.button_9,R.id.button_0, R.id.button__};
+        for (int i : a){
+            Button btn = (Button)findViewById(i);
+            btn.setOnClickListener(mClick);
+        }
+
+        Button btn = (Button)findViewById(R.id.button_);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String val = mTv.getText().toString();
+                if (val != null && val.length() > 0) {
+                    DB dbHelper = new DB(MainActivity.this);
+                    SQLiteDatabase db = dbHelper.getReadableDatabase();
+                    ContentValues cv = new ContentValues();
+                    cv.put(DB.T_DATA_[DB.T_DATA_TIME], DB.getDateTime(null));
+                    cv.put(DB.T_DATA_[DB.T_DATA_VALUE], val);
+                    cv.put(DB.T_DATA_[DB.T_DATA_DEINE], 0);
+
+                    db.insert(DB.T_DATA, null, cv);
+
+                    Toast.makeText(MainActivity.this, "insert into ", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
     }
 
 
